bf05f15ecff04c52566096118e4f3faa4640146e
haidukstore <hayduk.roman@gmail.com>
2020-02-19 14:27:45 +0200

    addd

--- src/com/rdruzhchenko/PizzaCalculator.java
+++ src/com/rdruzhchenko/PizzaCalculator.java
@@ -1,16 +1,42 @@
 package com.rdruzhchenko;
 
+import java.util.ArrayList;
+
 public class PizzaCalculator {
+
+    Integer[] array;
 
     public OutputData calculate(InputData inputData) {
 
         OutputData outputData = new OutputData();
+        array = inputData.slices;
+        var needed = inputData.maxSlices;
 
-        // Iterate
-        // If sum == max
-        //  return indexes
+        int n = array.length;
+        int dp[][] = new int[needed + 1][n + 1];
+        for (int j = 1; j <= n; j++) {
+            for (int w = 1; w <= needed; w++) {
+                if (array[j - 1] <= w) {
+                    dp[w][j] = Math.max(dp[w][j - 1], dp[w - array[j - 1]][j - 1] + array[j - 1]);
+                } else {
+                    dp[w][j] = dp[w][j - 1];
+                }
+            }
+        }
 
-        outputData.pizzasIndexes = new Integer[] { 0, 2, 3 };
+        ArrayList<Integer> list = new ArrayList();
+        var res = dp[needed][n];
+        System.out.println(res);
+        System.out.println();
+        for (int i = n; i > 0 && res > 0 ; i--) {
+            if (res != dp[needed][i-1]) {
+                list.add(i-1);
+                res = res - array[i-1];
+                needed = needed - array[i-1];
+            }
+        }
+
+        outputData.pizzasIndexes = (Integer[])list.toArray(new Integer[list.size()]);
         return outputData;
     }
 }
--- test/com/rdruzhchenko/PizzaCalculatorTest.java
+++ test/com/rdruzhchenko/PizzaCalculatorTest.java
@@ -2,6 +2,11 @@ package com.rdruzhchenko;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PizzaCalculatorTest {
@@ -39,5 +44,46 @@ class PizzaCalculatorTest {
         assertEquals(2, outputData.pizzasIndexes.length);
         assertEquals(18, outputData.pizzasIndexes[0]);
         assertEquals(82, outputData.pizzasIndexes[1]);
+    }
+
+    @Test
+    public void calculateC() {
+        InputData inputData = new InputData();
+        inputData.maxSlices = 4500;
+        inputData.slices = new Integer[] { 7, 12, 12, 13, 14, 28, 29, 29, 30, 32, 32, 34, 41, 45, 46, 56,
+                61, 61, 62, 63, 65, 68, 76, 77, 77, 92, 93, 94, 97, 103, 113, 114, 114, 120, 135, 145, 145, 149,
+                156, 157, 160, 169, 172, 179, 184, 185, 189, 194, 195, 195};
+        PizzaCalculator pizzaCalculator = new PizzaCalculator();
+
+        // When
+        OutputData outputData = pizzaCalculator.calculate(inputData);
+
+    }
+
+    @Test
+    public void calculateD() {
+        InputData inputData = new InputData();
+        try {
+
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+            File file = new File(classLoader.getResource("e_also_big.text").getFile());
+
+            Scanner scanner = new Scanner(file);
+
+            inputData.maxSlices = scanner.nextInt();
+            inputData.slices = new Integer[scanner.nextInt()];
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                inputData.slices[i++] = scanner.nextInt();
+            }
+            System.out.println(inputData.slices.length);
+
+            PizzaCalculator pizzaCalculator = new PizzaCalculator();
+
+            // When
+            OutputData outputData = pizzaCalculator.calculate(inputData);
+        } catch (Exception ex)  { }
+
     }
 }
